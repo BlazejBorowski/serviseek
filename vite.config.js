59f6a1aa6f1251399d04cbd8a9da1896f157eedf
BlazejBorowski <blazejborowski06@gmail.com>
2024-10-27 23:42:12 +0100

    update profile pages architecture and vite error repair

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import fg from "fast-glob";
+
+const pageInputs = fg.sync("resources/js/Pages/**/index.tsx");
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
-            ssr: 'resources/js/ssr.tsx',
+            input: ["resources/js/app.tsx", ...pageInputs],
+            ssr: "resources/js/ssr.tsx",
             refresh: true,
         }),
         react(),
